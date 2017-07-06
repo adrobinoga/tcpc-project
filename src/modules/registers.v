eceaf7d4bf8079fdab968efee411e744e6fc94b5
Alexander Marin <alexanderm2230@gmail.com>
2017-07-06 08:35:57 -0600

    solved sintax errors in tx

--- src/modules/registers.v
+++ src/modules/registers.v
@@ -1,129 +1,127 @@
-module registro
-(input clk,
-input RNW,			//indica si es read o write
+module registros(
+	input CLK,
+	input RNW,			//indica si es read o write
+	//separamos el write de entrada en 2 bytes porque hay algunos registros de 16 bits y otros de 8.
+	input [15:0] WR_DATA,		// 16 bits de entrada
+	// primeros 8 bits (izquierda) que van a escribir en un registro
+	// segundos 8 bits (derecha) que va a escribir en un registro
+	output reg [15:0] RD_DATA,	//registro salida que va a leer a un registro
+	input REQUEST,			//solicitud para hacer una transferencia
+	input [7:0] ADDR,		//direccion del registro
+ 	output reg ACK,
 
-//separamos el write de entrada en 2 bytes porque hay algunos registros de 16 bits y otros de 8.
-input [15:0] WR_DATA,			// 16 bits de entrada
-		// primeros 8 bits (izquierda) que van a escribir en un registro
-		// segundos 8 bits (derecha) que va a escribir en un registro
+	//---------------------------REGISTROS-----------------------------------------//
 
-output reg [15:0] RD_DATA,		//registro salida que va a leer a un registro
+	output reg [15:0] DEVICE_ID,      			//R              		
+	output reg [15:0] USBTYPEC_REV,				//R	    			
+	output reg [15:0] USBPD_REV_VER,			//R    		
+	output reg [15:0] PD_INTERFACE_REV,			//R   		
 
-input REQUEST,			//solicitud para hacer una transferencia
-input [7:0] ADDR,		//direccion del registro
- 
-output reg ACK,
+	//output reg [15:0] Reserved,				no dice de que tipo		    	
+	output reg [15:0] ALERT,				//RW    		
+	output reg [15:0] ALERT_MASK,				//RW    		
+	output reg [7:0] POWER_STATUS_MASK,			//RW    	
+	output reg [7:0] FAULT_STATUS_MASK,			//RW    	
+	  		
+	output reg [7:0] TCPC_CONTROL,				//RW    		
+	output reg [7:0] ROLE_CONTROL,				//RW    		
+	output reg [7:0] FAULT_CONTROL,				//RW    		
+	output reg [7:0] POWER_CONTROL,				//RW    		
+	output reg [7:0] CC_STATUS,				//R    		
+	output reg [7:0] POWER_STATUS,				//R    		
+	output reg [7:0] FAULT_STATUS,				//RW    		
 
-
-
-//---------------------------REGISTROS-----------------------------------------//
-
-output reg [15:0] DEVICE_ID,      			//R              		
-output reg [15:0] USBTYPEC_REV,				//R	    			
-output reg [15:0] USBPD_REV_VER,			//R    		
-output reg [15:0] PD_INTERFACE_REV,			//R   		
-
-//output reg [15:0] Reserved,				no dice de que tipo		    	
-output reg [15:0] ALERT,				//RW    		
-output reg [15:0] ALERT_MASK,				//RW    		
-output reg [7:0] POWER_STATUS_MASK,			//RW    	
-output reg [7:0] FAULT_STATUS_MASK,			//RW    	
-  		
-output reg [7:0] TCPC_CONTROL,				//RW    		
-output reg [7:0] ROLE_CONTROL,				//RW    		
-output reg [7:0] FAULT_CONTROL,				//RW    		
-output reg [7:0] POWER_CONTROL,				//RW    		
-output reg [7:0] CC_STATUS,				//R    		
-output reg [7:0] POWER_STATUS,				//R    		
-output reg [7:0] FAULT_STATUS,				//RW    		
-
-output reg [15:0] Reserved,				//R 		    		
-output reg [7:0] COMMAND,				//W		    		
-output reg [15:0] DEVICE_CAPABILITIES_1, 		//R 	  		
-output reg [15:0] DEVICE_CAPABILITIES_2, 		//R   		
-output reg [7:0] STANDARD_INPUT_CAPABILITIES, 		//R  	
-output reg [15:0] STANDARD_OUTPUT_CAPABILITIES, 	//R  		
+	output reg [15:0] Reserved,				//R 		    		
+	output reg [7:0] COMMAND,				//W		    		
+	output reg [15:0] DEVICE_CAPABILITIES_1, 		//R 	  		
+	output reg [15:0] DEVICE_CAPABILITIES_2, 		//R   		
+	output reg [7:0] STANDARD_INPUT_CAPABILITIES, 		//R  	
+	output reg [15:0] STANDARD_OUTPUT_CAPABILITIES, 	//R  		
 				
-output reg [7:0] MESSAGE_HEADER_INFO, 			//RW		
-output reg [7:0] RECEIVE_DETECT, 			//RW		
-output reg [7:0] RECEIVE_BYTE_COUNT, 			//R 		
-output reg [7:0] RX_BUF_FRAME_TYPE, 			//R	
-output reg [7:0] RX_BUF_HEADER_BYTE_0, 			//R 		
-output reg [7:0] RX_BUF_HEADER_BYTE_1,			//R  	
-output reg [7:0] RX_BUF_OBJ1_BYTE_0, 			//R 		
-output reg [7:0] RX_BUF_OBJ1_BYTE_1,			//R  		
-output reg [7:0] RX_BUF_OBJ1_BYTE_2, 			//R 		
-output reg [7:0] RX_BUF_OBJ1_BYTE_3,			//R 	
-output reg [7:0] RX_BUF_OBJ2_BYTE_0, 			//R 		
-output reg [15:0] RX_BUF_OBJn_BYTE_m,			//R  		
-output reg [7:0] RX_BUF_OBJ7_BYTE_3, 			//R 		
+	output reg [7:0] MESSAGE_HEADER_INFO, 			//RW		
+	output reg [7:0] RECEIVE_DETECT, 			//RW		
+	output reg [7:0] RECEIVE_BYTE_COUNT, 			//R 		
+	output reg [7:0] RX_BUF_FRAME_TYPE, 			//R	
+	output reg [7:0] RX_BUF_HEADER_BYTE_0, 			//R 		
+	output reg [7:0] RX_BUF_HEADER_BYTE_1,			//R  	
+	output reg [7:0] RX_BUF_OBJ1_BYTE_0, 			//R 		
+	output reg [7:0] RX_BUF_OBJ1_BYTE_1,			//R  		
+	output reg [7:0] RX_BUF_OBJ1_BYTE_2, 			//R 		
+	output reg [7:0] RX_BUF_OBJ1_BYTE_3,			//R 	
+	output reg [7:0] RX_BUF_OBJ2_BYTE_0, 			//R 		
+	output reg [15:0] RX_BUF_OBJn_BYTE_m,			//R  		
+	output reg [7:0] RX_BUF_OBJ7_BYTE_3, 			//R 		
 
-output reg [7:0] TRANSMIT,				//RW	
-output reg [7:0] TRANSMIT_BYTE_COUNT, 			//RW	
-output reg [7:0] TX_BUF_HEADER_BYTE_0, 			//RW
-output reg [7:0] TX_BUF_HEADER_BYTE_1, 			//RW	
-output reg [7:0] TX_BUF_OBJ1_BYTE_0, 			//RW	
-output reg [15:0] TX_BUF_OBJn_BYTE_m, 			//RW		
-output reg [7:0] TX_BUF_OBJ7_BYTE_3,  			//RW		
+	output reg [7:0] TRANSMIT,				//RW	
+	output reg [7:0] TRANSMIT_BYTE_COUNT, 			//RW	
+	output reg [7:0] TX_BUF_HEADER_BYTE_0, 			//RW
+	output reg [7:0] TX_BUF_HEADER_BYTE_1, 			//RW	
+	output reg [7:0] TX_BUF_OBJ1_BYTE_0, 			//RW	
+	output reg [15:0] TX_BUF_OBJn_BYTE_m, 			//RW		
+	output reg [7:0] TX_BUF_OBJ7_BYTE_3,  			//RW		
 
-output reg [15:0] VBUS_VOLTAGE, 			//R 			
-output reg [15:0] VBUS_SINK_DISCONNECT_THRESHOLD, 	//RW 	
-output reg [15:0] VBUS_STOP_DISCHARGE_THRESHOLD,
-output reg [15:0] VBUS_SINK_DISCHARGE_THRESHOLD, 	//RW
-output reg [15:0] VBUS_VOLTAGE_ALARM_HI_CFG,		//RW
-output reg [15:0] VBUS_VOLTAGE_ALARM_LO_CFG); 		//RW
+	output reg [15:0] VBUS_VOLTAGE, 			//R 			
+	output reg [15:0] VBUS_SINK_DISCONNECT_THRESHOLD, 	//RW 	
+	output reg [15:0] VBUS_STOP_DISCHARGE_THRESHOLD,
+	output reg [15:0] VBUS_SINK_DISCHARGE_THRESHOLD, 	//RW
+	output reg [15:0] VBUS_VOLTAGE_ALARM_HI_CFG,		//RW
+	output reg [15:0] VBUS_VOLTAGE_ALARM_LO_CFG); 		//RW
 				
-
-
-//SEPARAR ESCRITURA EN DOS BYTES
-
-
 
 //el request debe estar en 1 para que se realice un write o un read, si esta en 0 no pasa nada
 //se hace un case entre los que son de lectura y escritura. Si RNW=1---> lectura, si RNW=0--> escritura
 //el case lo que busca es el address de cada registro
 //TODAS las direcciones son de 8 bits, 
 
-always @(posedge clk) 	begin
-ACK <= 0 ;  //el ack solo dure un ciclo
-if (REQUEST)	begin
-	if (RNW)	begin 	//si se trata de una lectura
-	case(ADDR)
-
-			//direccion: RD_DATA <= nombre del registro
-
-				00000100: begin 
+always @(posedge CLK)
+ 	begin
+	ACK <= 0 ;  //el ack solo dure un ciclo
+	if (REQUEST)	
+		begin
+		if (RNW)
+			begin 	//si se trata de una lectura
+			case(ADDR) //direccion: RD_DATA <= nombre del registro
+			
+				00000100: 
+					begin 
 					RD_DATA <= DEVICE_ID;
 					ACK <= 1;	
-					  end	
-				00000101: begin				
+					end	
+				00000101: 
+					begin				
 					RD_DATA <= DEVICE_ID; 
 					ACK <= 1;
-					  end		
-				00000110: begin
+					end		
+				00000110: 
+					begin
 					RD_DATA <= USBTYPEC_REV;
 					ACK <= 1;
-					  end
-				00000111: begin
+					end
+				00000111: 
+					begin
 					RD_DATA <= USBTYPEC_REV;
 					ACK <= 1;	
-					  end
-				00001000: begin 
+					end
+				00001000: 
+					begin 
 					RD_DATA <= USBPD_REV_VER;
 					ACK <= 1;
-					  end
-				00001001: begin
-					 RD_DATA <= USBPD_REV_VER;
-       					 ACK <= 1;	
-					  end
-				00001010: begin
-					 RD_DATA <= PD_INTERFACE_REV;
-					 ACK <= 1;	
-					  end
-				00001011: begin
-					 RD_DATA <= PD_INTERFACE_REV;
-					 ACK <= 1;	
-					  end
+					end
+				00001001: 
+					begin
+					RD_DATA <= USBPD_REV_VER;
+       					ACK <= 1;	
+					end
+				00001010: 
+					begin
+					RD_DATA <= PD_INTERFACE_REV;
+					ACK <= 1;	
+					end
+				00001011: 
+					begin
+					RD_DATA <= PD_INTERFACE_REV;
+					ACK <= 1;	
+					end
 				00010000: RD_DATA <= ALERT;
 				00010001: RD_DATA <= ALERT;
 
@@ -165,8 +163,9 @@ if (REQUEST)	begin
 				//XXXXXXXX: RD_DATA <= RX_BUF_OBJn_BYTE_m;
 				01001111: RD_DATA <= RX_BUF_OBJ7_BYTE_3;
 				01010000: RD_DATA <= TRANSMIT;			//50
-				8'h51: begin
-					 RD_DATA <= TRANSMIT_BYTE_COUNT;   	//51
+				8'h51: 
+					begin
+					RD_DATA <= TRANSMIT_BYTE_COUNT;   	//51
 					ACK <= 1;
 					end
 				01010010: RD_DATA <= TX_BUF_HEADER_BYTE_0;
@@ -189,12 +188,11 @@ if (REQUEST)	begin
 
 				01111000: RD_DATA <= VBUS_VOLTAGE_ALARM_LO_CFG;
 				01111001: RD_DATA <= VBUS_VOLTAGE_ALARM_LO_CFG;
-		
-	endcase
+			endcase
 			end	//end if(RNW)
-	else		
+		else		
 		begin		
-	case (ADDR)		//si se trata de una escritura
+			case (ADDR) //si se trata de una escritura
 		
 				00010000: begin
 					 ALERT <= WR_DATA;
@@ -248,10 +246,8 @@ if (REQUEST)	begin
 	
 				01111000: VBUS_VOLTAGE_ALARM_LO_CFG <= WR_DATA;
 				01111001: VBUS_VOLTAGE_ALARM_LO_CFG <= WR_DATA;
-
-	endcase
-		end 	//end else
-			end	//end if(request)
-		end	//end always
-    
-  endmodule
+			endcase
+			end 	//end else
+		end	//end if(request
+	end	//end always
+endmodule
--- src/modules/tx.v
+++ src/modules/tx.v
@@ -5,20 +5,19 @@ module tx(
 	);
 
 //cantidad de bits por cantidad de estados que se tiene
+localparam Tx_Wait_for_Transmit_Request = 	8'b00000001;
+localparam Tx_Reset_RetryCounter = 		8'b00000010;
+localparam Tx_Check_RetryCounter = 		8'b00000100;
+localparam Tx_Construct_Message = 		8'b00001000;
+localparam Tx_Wait_for_PHY_response = 		8'b00010000;
+localparam Tx_Report_Failure = 			8'b00100000;
+localparam Tx_Report_Success = 			8'b01000000;
+localparam Tx_Match_MessageID = 		8'b10000000;
 
-localparam Tx_Wait_for_Transmit_Request = 8'b00000000;
-localparam Tx_Reset_RetryCounter = 8'b00000010;
-localparam Tx_Check_RetryCounter = 8'b00000100;
-localparam Tx_Construct_Message = 8'b00001000;
-localparam Tx_Wait_for_PHY_response = 8'b00010000;
-localparam Tx_Report_Failure = 8'b00100000;
-localparam Tx_Report_Success = 8'b01000000;
-localparam Tx_Match_MessageID = 8'b10000000;
+reg[7:0] current_state;
+reg[7:0] next_state;
 
-//ff(clock)
-//logica prox estado(*)
-//logica salida always(*)
-
+/*
 reg MID_Match;
 reg MessageSentToPhy;
 reg GoodCRCResponse;
@@ -32,8 +31,7 @@ reg TX_BUF_HEADER_BYTE_1;
 reg RX_BUF_HEADER_BYTE_1;
 reg[2:0] RX_BUF_FRAME_TYPE;
 
-reg[2:0] CurrentState;
-reg[2:0] NextState;
+
 reg[2:0] TRANSMIT;
 
 input wire DFP, UFP;
@@ -43,46 +41,82 @@ input wire MessageID;
 output reg InitCRCReceiveTimer;
 output reg Alert_MessageSuccessful;
 output reg Alert_MessageFailed;
-
-always@(*) begin
-  NextState = CurrentState;
-  case (CurrentState)
-    default: NextState = CurrentState;
-    Tx_Wait_for_Transmit_Request: begin
-      if(TRANSMIT < 3'b101) NextState = Tx_Reset_RetryCounter;
-      else NextState = Tx_Wait_for_Transmit_Request;
-      end
-    Tx_Reset_RetryCounter: begin
-      NextState =  Tx_Construct_Message;
-      end
-    Tx_Check_RetryCounter: begin
-        if (RetryCounter > nRetryCount) NextState = Tx_Report_Failure;
-        else NextState = Tx_Construct_Message;
-      end
-    Tx_Construct_Message: begin
-        if (MessageSentToPhy) NextState = Tx_Wait_for_PHY_response;
-        else NextState = Tx_Construct_Message;
-      end
-    Tx_Wait_for_PHY_response: begin
-        if (GoodCRCResponse) NextState = Tx_Match_MessageID;
-        else if (CRC_RT_Timeout | MessageDiscardedBusIdle) NextState =  Tx_Check_RetryCounter;
-      end
-    Tx_Report_Failure: begin
-        NextState = Tx_Wait_for_Transmit_Request;
-      end
-    Tx_Report_Success: begin
-      NextState = Tx_Wait_for_Transmit_Request;
-      end
-    Tx_Match_MessageID: begin
-      if ((TX_BUF_HEADER_BYTE_1 != RX_BUF_HEADER_BYTE_1) | (TRANSMIT != RX_BUF_FRAME_TYPE)) NextState = Tx_Check_RetryCounter;
-      else if (GoodCRCMessageID & SOPmatch) NextState = Tx_Report_Success;
-      end
-  endcase
-end
+*/
 
 
+// logica de proximo estado
+always@(*) 
+	begin
+	next_state = current_state;
+	case (current_state)
+
+	Tx_Wait_for_Transmit_Request: 
+		begin
+		if(TRANSMIT[2:0] < 3'b101) 
+			next_state = Tx_Reset_RetryCounter;
+		else 
+			next_state = Tx_Wait_for_Transmit_Request;
+		end
+		
+	Tx_Reset_RetryCounter: 
+		begin
+		next_state =  Tx_Construct_Message;
+		end
+	
+	Tx_Check_RetryCounter: 
+		begin
+		if (RetryCounter > nRetryCount) 
+			next_state = Tx_Report_Failure;
+		else 
+			next_state = Tx_Construct_Message;
+		end
+	
+	Tx_Construct_Message: 
+		begin
+		if (MessageSentToPhy) 
+			next_state = Tx_Wait_for_PHY_response;
+		else 
+			next_state = Tx_Construct_Message;
+		end
+	
+	Tx_Wait_for_PHY_response: 
+		begin
+		if (GoodCRCResponse) 
+			next_state = Tx_Match_MessageID;
+		else 
+			if (CRC_RT_Timeout | MessageDiscardedBusIdle) 
+				next_state =  Tx_Check_RetryCounter;
+		end
+	
+	Tx_Report_Failure: 
+		begin
+		next_state = Tx_Wait_for_Transmit_Request;
+		end
+	
+	Tx_Report_Success: 
+		begin
+		next_state = Tx_Wait_for_Transmit_Request;
+		end
+	
+	Tx_Match_MessageID: 
+		begin
+		if ((TX_BUF_HEADER_BYTE_1 != RX_BUF_HEADER_BYTE_1) |
+			 (TRANSMIT != RX_BUF_FRAME_TYPE)) 
+			 next_state = Tx_Check_RetryCounter;
+		else 
+			if (GoodCRCMessageID & SOPmatch) 
+				next_state = Tx_Report_Success;
+		end
+		
+	default: next_state = current_state;
+	
+	endcase
+	end
+
+
+// logica de salidas
 always@ (*) begin
-	case (CurrentState)
+	case (current_state)
 		Tx_Reset_RetryCounter: begin
 			RetryCounter = 0;
 		end
@@ -110,13 +144,13 @@ always@ (*) begin
 	endcase
 end
 
-
+// memoria de estado
 always @(posedge clk) 
 	begin
 	if (reset)
-		CurrentState <= Tx_Wait_for_Transmit_Request;
+		current_state <= Tx_Wait_for_Transmit_Request;
 	else 
-		CurrentState <= NextState;
+		current_state <= next_state;
 	end
 	
 endmodule
