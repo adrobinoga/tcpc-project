da80b960794827013591be0695b0c6d083c4b8a7
Alexander Marin <alexanderm2230@gmail.com>
2017-07-11 15:52:33 -0600

    syntax hard reset

--- src/modules/hard_reset.v
+++ src/modules/hard_reset.v
@@ -1,22 +1,13 @@
 module HardResetTransmission(
-		clk, hard_reset_L, cable_reset_L, max_reset_timer, phy_response,
-
-		transmit, phy_request, ALERT_TransmitSuccessful, ALERT_TransmitSOPMessageFailed );
-
-input		clk;
-input		hard_reset_L;
-input		cable_reset_L;
-input	[31:0]  max_reset_timer;
-input		phy_response;
-
-output reg [2:0] transmit;
-output reg phy_request;
-
-output reg ALERT_TransmitSuccessful;
-output reg ALERT_TransmitSOPMessageFailed;	//SOP*
-//output reg [15:0] ALERT;
-
-
+		input clk,
+		input hard_reset_L, 
+		input cable_reset_L, 
+		input [31:0] max_reset_timer, 
+		input phy_response,
+		input [7:0] TRANSMIT, 
+		output reg phy_request, 
+		output reg ALERT_TransmitSuccessful, 
+		output reg ALERT_TransmitSOPMessageFailed);
 
 reg [31:0]	hard_reset_complete_timer;
 
@@ -45,68 +36,73 @@ assign expired_timer = ~| hard_reset_complete_timer;
 assign pu_reset_L = ~cable_reset_L || ~hard_reset_L ;
 ///////////////////////////////////////////////////////////////////
 
-//FLIP-FLOPS, ya esta listo
 
-
-always @(posedge clk) begin
-
-	if(~pu_reset_L) begin
-
+//FLIP-FLOPS
+always @(posedge clk) 
+	begin
+	if(~pu_reset_L) 
+		begin
 		state <= HR_WAIT_FOR_HARD_RESET_REQUEST;	//si el reset es 0, quedese en el primer estado y transmit=0
 		transmit <= 3'b000;
 		hard_reset_complete_timer <= max_reset_timer;
-
-			end
-	else	begin			//si el reset es 1, pasa al siguiente estado
-
+		end
+		
+	else	
+		begin			//si el reset es 1, pasa al siguiente estado
 		state <= next_state;
 		transmit <= next_transmit;
 		hard_reset_complete_timer <= next_hard_reset_complete_timer;					
-		
 	     	end
+	end
 
-			end
 
-//proximo estado, ya esta listo
-
-always @(*) 	begin
+// logica de proximo estado
+always @(*) 	
+	begin
 	case(state)	
-	   HR_WAIT_FOR_HARD_RESET_REQUEST:				//estado 1 esperar pedido de reset
-			if(pu_reset_L) //creo que hay que cambiarlo a que cuando sea 1
-			   begin
-				next_state = HR_CONSTRUCT_MESSAGE;
-			   end
-			else begin
-				next_state = HR_WAIT_FOR_HARD_RESET_REQUEST;
-			     end
+	HR_WAIT_FOR_HARD_RESET_REQUEST:	//estado 1 esperar pedido de reset
+		begin
+		if(pu_reset_L) 
+			begin
+			next_state = HR_CONSTRUCT_MESSAGE;
+			end
+		else 
+			begin
+			next_state = HR_WAIT_FOR_HARD_RESET_REQUEST;
+			end
+		end
 
-	   HR_CONSTRUCT_MESSAGE:				//estado 2 construir mensaje
-			if(phy_response)
-			   begin
-				next_state = HR_SUCCESS;
-			   end
-			else if(expired_timer) 
-			   begin
+	HR_CONSTRUCT_MESSAGE:	//estado 2 construir mensaje
+		begin
+		if(phy_response)
+			begin
+			next_state = HR_SUCCESS;
+			end
+		else 
+			if(expired_timer) 
+				begin
 				next_state = HR_FAILURE;
-                           end
+                   		end
 			else 
 				next_state = HR_CONSTRUCT_MESSAGE;
+		end
 
-	   HR_FAILURE:						//estado 3 fallo
-			   begin
-				next_state = HR_REPORT;
-			   end
-	   HR_SUCCESS:						//estado 4 éxito
-			   begin
-				next_state = HR_REPORT;
-			   end		
-	   HR_REPORT:						//estado 5 mandar reporte
-
-			   begin
-				next_state = HR_WAIT_FOR_HARD_RESET_REQUEST;
-			   end	
+	HR_FAILURE:	//estado 3 fallo
+		begin
+		next_state = HR_REPORT;
+		end
+			   
+	HR_SUCCESS:	//estado 4 éxito
+		begin
+		next_state = HR_REPORT;
+		end
+			   		
+	HR_REPORT:	//estado 5 mandar reporte
+		begin
+		next_state = HR_WAIT_FOR_HARD_RESET_REQUEST;
+		end	
 	endcase
-   		end
+   	end
 
 //LOGICA DE SALIDAS
 
