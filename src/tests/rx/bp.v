6ae0c40be1c6799696bcc534cbc4ca86e733879d
alerojco <alerojco@gmail.com>
2017-07-11 18:07:37 -0600

    test

---
+++ src/tests/rx/bp.v
@@ -0,0 +1,29 @@
+`include "rx.v" 
+`include "tb.v"
+
+module bp ();
+
+   wire clk;
+   wire hard_reset;
+   wire [7:0] MESSAGE_HEADER_INFO;
+   wire [7:0] TX_BUF_HEADER_BYTE_0;
+   wire [7:0] TX_BUF_HEADER_BYTE_1;
+   wire [7:0] RECEIVE_DETECT;
+   wire       phy_rx_goodcrc;
+   wire       GoodCRC_Transmission_Complete;
+   wire       RECEIVE_DETECT_retro;
+
+   
+   testbench testb(clk,hard_reset,MESSAGE_HEADER_INFO,TX_BUF_HEADER_BYTE_0,TX_BUF_HEADER_BYTE_1,RECEIVE_DETECT,phy_rx_goodcrc,GoodCRC_Transmission_Complete,RECEIVE_DETECT_retro);   
+   rx Rx(clk,hard_reset,,,,MESSAGE_HEADER_INFO,RECEIVE_DETECT,TX_BUF_HEADER_BYTE_1,TX_BUF_HEADER_BYTE_0,,,,GoodCRC_Transmission_Complete,phy_rx_goodcrc,,,,,,,,,,RECEIVE_DETECT_retro);
+
+    initial
+     
+     begin
+	$dumpfile("simulacion_Rx.vcd");
+	$dumpvars;
+	$display ("Simulación");
+	$monitor ($time,,"clk = %b", clk);
+	#2000 $finish;
+     end
+endmodule  
---
+++ src/tests/rx/tb.v
@@ -0,0 +1,42 @@
+module testbench (
+		 output reg 	  clk,
+		 output reg 	  hard_reset,
+		 output reg [7:0] MESSAGE_HEADER_INFO,
+		 output reg [7:0] TX_BUF_HEADER_BYTE_0,
+		 output reg [7:0] TX_BUF_HEADER_BYTE_1,
+		 output reg [7:0] RECEIVE_DETECT,
+		 output reg 	  phy_rx_goodcrc,
+		 output reg 	  GoodCRC_Transmission_Complete,
+		 input wire 	  RECEIVE_DETECT_IN
+		 );
+
+   initial
+     begin
+	MESSAGE_HEADER_INFO=8'b00011010;       
+	clk=0;
+	hard_reset=1;
+	#15 hard_reset=0;
+	#30 TX_BUF_HEADER_BYTE_0=8'b01001011;
+	TX_BUF_HEADER_BYTE_1=8'b00100111;
+	#10 RECEIVE_DETECT=8'b00000001;
+	phy_rx_goodcrc=1;
+	#40 GoodCRC_Transmission_Complete=1;
+	
+	
+	
+
+	
+     end
+
+   always
+     begin
+	#10 clk = ~clk;	
+	
+     end
+
+   always @ (RECEIVE_DETECT_IN)
+     begin
+	RECEIVE_DETECT<=RECEIVE_DETECT_IN;
+	
+     end
+endmodule
